202e1b003181abf23e45e07d59ea9fa379a6b251
Manhar-Dangar <73153726+Manhar-Dangar@users.noreply.github.com>
2022-05-10 15:45:48 +0530

    Create main.tf

---
+++ prod/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "my_vpc" {
+  source      = "../modules/vpc"
+  vpc_cidr    = "192.168.0.0/16"
+  tenancy     = "default"
+  vpc_id      = "${module.my_vpc.vpc_id}"
+  subnet_cidr = "192.168.1.0/24"
+  vpctag      = "prodmyapp"
+}
+
+module "my_ec2" {
+  source        = "../modules/ec2"
+  ec2_count     = 1
+  ec2_type      = "t3.micro"
+  subnet_id     = "${module.my_vpc.subnet_id}"
+  ec2_tag       = "my_prod_ec2"
+}
