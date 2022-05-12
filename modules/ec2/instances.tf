09e092c3fd56509f02175964f6b2126a3bba3f6e
Manhar-Dangar <73153726+Manhar-Dangar@users.noreply.github.com>
2022-05-12 11:44:04 +0530

    Update instances.tf

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -3,7 +3,14 @@ resource "aws_instance" "app" {
   ami           = "${var.ami_id}"
   instance_type = "${var.ec2_type}"
   subnet_id     = "${var.subnet_id}"
-
+ user_data = <<EOF
+    #! /bin/bash
+                sudo yum update -y
+    sudo yum install -y httpd
+    sudo service httpd start
+    sudo service httpd enable
+    echo "<h1>Hello World</h1>" | sudo tee /var/www/html/index.html
+  EOF
   tags = {
     Name = "${var.ec2_tag}"
   }  
